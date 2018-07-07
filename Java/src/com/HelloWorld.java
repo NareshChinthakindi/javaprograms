222201d08b7b64f89c642cf56bd2c4227667455b
Naresh_Chinthakindi <Naresh_Chinthakindi@EPINHYDW1086.princeton.epam.com>
2018-07-07 18:00:41 +0530

    First Time Commit

---
+++ Java/src/com/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com;
+
+/**
+ * @author Naresh_Chinthakindi
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HelloWorld");
+	}
+
+}
